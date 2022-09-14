ced04ac21f5ca92e7e8ed8a743f5f65c93cd7475
Dipson <79448381+Dipsonn@users.noreply.github.com>
2022-09-14 16:19:19 -0700

    Visualization pretty much completed (very basic tho)

--- Biodiversity Project/FishFrenzy_Array_VIS_FOR_TK.py
+++ Biodiversity Project/FishFrenzy_Array_VIS_FOR_TK.py
@@ -4,17 +4,29 @@
 Version of Sep. 13, 2022.
 '''
 
+from ast import operator
 from show_state_array import initialize_tk, state_array, state_display, STATE_WINDOW, test
 
 from tkinter import font
 from tkinter import *
 from tkinter import ttk
-import sys
+import sys, importlib.util
+
+try:
+  spec = importlib.util.spec_from_file_location("FishFrenzy", "FishFrenzy"+".py")
+  PROBLEM = spec.loader.load_module()
+  spec.loader.exec_module(PROBLEM)
+except Exception as e:
+  print(e)
+  exit(1)
+
+
 myFont=None
 
-WIDTH = 400
-HEIGHT = 400
-TITLE = 'FishFrenzy'
+WIDTH = 580
+HEIGHT = 530
+TITLE = 'Fishing Frenzy'
+
 
 def initialize_vis():
   initialize_tk(WIDTH, HEIGHT, TITLE)
@@ -25,7 +37,7 @@ def render_state(s):
     # and we do it (the first time this method is called).
     global myFont
     if not myFont:
-        myFont = font.Font(family="Helvetica", size=18, weight="bold")
+        myFont = font.Font(family="Helvetic", size=10, weight="bold")
     print("In render_state, state is "+str(s))
     # Create the default array of colors
     white = (255, 255, 255)
@@ -35,15 +47,17 @@ def render_state(s):
     halibutColor = (184, 115, 51)
     pompanoColor = (255, 243, 128)
     codColor = (170, 255, 0)
+    blue = (25, 25, 112)
     
-    row = [white]*6
+
+    row = [white]*7
     the_color_array = [row]
-    for i in range(9):
+    for i in range(10):
         the_color_array.append(row[:])
     # Now create the default array of string labels.
-    row = ['' for i in range(6)]
+    row = ['' for i in range(7)]
     the_string_array = [row]
-    for j in range(9):
+    for j in range(10):
         the_string_array.append(row[:])
 
     # Adjust colors and strings to match the state.
@@ -53,12 +67,59 @@ def render_state(s):
         fishBars = int(fish.number / 1000)
         fishTotal += fish.number
         colorList = [salmonColor, tunaColor, codColor, pompanoColor, bassColor, halibutColor]
+        
+
+        #Vertical colors for margins
+        the_color_array[0][0] = blue
+        the_color_array[1][0] = blue
+        the_color_array[2][0] = blue
+        the_color_array[3][0] = blue
+        the_color_array[4][0] = blue
+        the_color_array[5][0] = blue
+        the_color_array[6][0] = blue
+        the_color_array[7][0] = blue
+        the_color_array[8][0] = blue
+        the_color_array[9][0] = blue
+        the_color_array[10][0] = blue
+
+        #Lables for the table to help the player know how many fish there are
+        the_string_array[0][0] = '10,000'
+        the_string_array[1][0] = '9,000'
+        the_string_array[2][0] = '8,000'
+        the_string_array[3][0] = '7,000'
+        the_string_array[4][0] = '6,000'
+        the_string_array[5][0] = '5,000'
+        the_string_array[6][0] = '4,000'
+        the_string_array[7][0] = '3,000'
+        the_string_array[8][0] = '2,000'
+        the_string_array[9][0] = '1,000'
+
+        #Horizontal colors for margins
+        the_color_array[10][1] = blue
+        the_color_array[10][2] = blue
+        the_color_array[10][3] = blue
+        the_color_array[10][4] = blue
+        the_color_array[10][5] = blue
+        the_color_array[10][6] = blue
+
+        #Fish Names in boxes at bottom of grid
+        the_string_array[10][1] = s.fishList[0].name +'\n' + str(s.fishList[0].number)
+        the_string_array[10][2] = s.fishList[1].name +'\n' + str(s.fishList[1].number)
+        the_string_array[10][3] = s.fishList[2].name +'\n' + str(s.fishList[2].number)
+        the_string_array[10][4] = s.fishList[3].name +'\n' + str(s.fishList[3].number)
+        the_string_array[10][5] = s.fishList[4].name +'\n' + str(s.fishList[4].number)
+        the_string_array[10][6] = s.fishList[5].name +'\n' + str(s.fishList[5].number)
+
+
         for i in range(fishBars):
-            the_color_array[9 - i][j] = colorList[j]
+            the_color_array[9 - i][j + 1] = colorList[j]
+            
+
         j+=1
 
 
-    caption="Current state of the puzzle. Textual version: "+str(s)        
+    caption="Current state of the puzzle. Textual version: "+str(s)   
+
     the_state_array = state_array(color_array=the_color_array,
                                   string_array=the_string_array,
                                   text_font=myFont,
@@ -67,8 +128,3 @@ def render_state(s):
     the_state_array.show()
     #if fishTotal <= 0:
         #sys.exit()
-
-    
-    
-
-    
